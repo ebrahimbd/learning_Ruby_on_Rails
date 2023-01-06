dc0247c6c67d70a59a5a7567d5cdbf412c808203
ebrahimbd <ebrahim16sust@gmail.com>
2023-01-06 23:56:21 +0600

    learn more rails related topic

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,21 @@
 class HomeController < ApplicationController
    protect_from_forgery with: :null_session
   def index
+   flash[:notice]=false
+  end
+
+  def comp
+     flash[:eb]="dsfsdfsdfdsfdsfdsfdsfdsf"
+     flash[:notice]="This is dynamic alert"
+     @student=Student.all()
+      @arr=[]
+     @student.each do |e|
+      @arr.push({
+         "name" =>e.first_name,
+         "last_naem" =>e.last_name,
+         "email"=>e.email,
+      })
+     end
+     puts  @arr[0]["name"]
   end
 end
